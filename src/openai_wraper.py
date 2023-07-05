8703e71101fa638fed687aaca794bbd9bd721aae
DandinPower <tomhot246@gmail.com>
2023-07-05 09:32:54 +0800

    add new test case

--- src/openai_wraper.py
+++ src/openai_wraper.py
@@ -2,34 +2,113 @@ from typing import List
 from collections import deque
 
 DEFAULT_CAPACITY = 1
+DEFAULT_SYSTEM_PROMPT = "You are a helpful assistant"
+DEFAULT_MAX_TOKENS = 512
+
+INVALID_MAX_TOKENS_HINT = "Invalid MaxTokens: MaxTokens must larger than zero and smaller than 2049!"
+INVALID_SYSTEM_PROMPT_HINT = "Invalid SystemPrompt: SystemPrompt can not be empty!"
+INVALID_CAPACITY_HINT = "Invalid Capacity: Capacity must larger than zero!"
+INVALID_USER_CONTENT = "Invalid UserContent: UserContent can not be empty!"
+INVALID_ASSISTANT_CONTENT = "Invalid AssistantContent: AssistantContent can not be empty!"
 
 class ChatInterface:
     def __init__(self) -> None:
+        self.systemPrompt = DEFAULT_SYSTEM_PROMPT
+        self.maxTokens = DEFAULT_MAX_TOKENS
         self.capacity = DEFAULT_CAPACITY
-        self.messages = deque(maxlen = self.capacity)
+        self.messages = deque(maxlen=self.capacity)
 
-    def SetCapacity(self, capacity:int) -> bool:
+    def SetMaxTokens(self, maxTokens: int) -> bool:
         """
-        SetCapacity will change the capacity of memory, \
-        *remember that SetCapacity will reset current Messages
+        SetMaxTokens sets the maximum number of tokens for the model's response.
 
-        :param capacity:int: the capacity value you want to change
-        :return: if change process successful return True, else will raise ValueError
-        """ 
+        :param maxTokens: The maximum number of tokens.
+        :return: True if the change process is successful.
+        :raises ValueError: If maxTokens is greater than 2048 or less than 1.
+        """
+        self.maxTokens = DEFAULT_SYSTEM_PROMPT
+        if maxTokens > 2048 or maxTokens < 1:
+            raise ValueError(INVALID_MAX_TOKENS_HINT)
+        self.maxTokens = maxTokens
+        return True
+
+    def SetSystemPrompt(self, systemPrompt: str) -> bool:
+        """
+        SetSystemPrompt changes the system prompt used by the assistant.
+
+        :param systemPrompt: The system prompt string to be changed.
+        :return: True if the change process is successful.
+        :raises ValueError: If systemPrompt is an empty string.
+        """
+        self.systemPrompt = DEFAULT_SYSTEM_PROMPT
+        if not systemPrompt:
+            raise ValueError(INVALID_SYSTEM_PROMPT_HINT)
+        self.systemPrompt = systemPrompt
+        return True
+
+    def SetCapacity(self, capacity: int) -> bool:
+        """
+        SetCapacity changes the capacity of the memory.
+        Note: SetCapacity will clear the current messages.
+
+        :param capacity: The new capacity value.
+        :return: True if the change process is successful.
+        :raises ValueError: If capacity is less than or equal to zero.
+        """
         self.capacity = DEFAULT_CAPACITY
         if capacity <= 0:
-            raise ValueError("Invalid Capacity: Capacity must larger than zero!")
+            raise ValueError(INVALID_CAPACITY_HINT)
         self.capacity = capacity
-        self.messages = deque(maxlen = self.capacity)
+        self.messages = deque(maxlen=self.capacity)
         return True
 
-    def AddMessageToMemory(self, userContent:str, assistantContent:str) -> bool:
-        self.messages.append({"userContent" : userContent, "assistantContent" : assistantContent})
-        return True 
-    
+    def AddMessageToMemory(self, userContent: str, assistantContent: str) -> bool:
+        """
+        AddMessageToMemory adds a user message and its corresponding assistant message to the memory.
+
+        :param userContent: The content of the user message.
+        :param assistantContent: The content of the assistant message.
+        :return: True if the message is successfully added to the memory.
+        :raises ValueError: If either userContent or assistantContent is an empty string.
+        """
+        if not userContent:
+            raise ValueError(INVALID_USER_CONTENT)
+        if not assistantContent:
+            raise ValueError(INVALID_ASSISTANT_CONTENT)
+        self.messages.append({"userContent": userContent, "assistantContent": assistantContent})
+        return True
+
     def GetMessagesFromMemory(self) -> List[dict]:
-        return self.messages
-    
+        """
+        GetMessagesFromMemory retrieves the messages stored in the memory.
+
+        :return: A list of dictionaries representing the stored messages.
+        """
+        return list(self.messages)
+
     def RemoveMessages(self) -> bool:
+        """
+        RemoveMessages clears all the messages stored in the memory.
+
+        :return: True if the messages are successfully removed from the memory.
+        """
         self.messages.clear()
         return True
+    
+    def MessagePreprocess(self, newUserContent:str) -> List[dict]:
+        """
+        MessagePreprocess preprocesses the user's new content along with the existing messages.
+
+        :param newUserContent: The content of the new user message.
+        :return: A list of dictionaries representing the preprocessed messages.
+        :raises ValueError: If newUserContent is an empty string.
+        """
+        if not newUserContent:
+            raise ValueError(INVALID_USER_CONTENT)
+        messages = [{"role":"system", "content":self.systemPrompt}]
+        for message in self.messages:
+            messages.append({"role":"user", "content":message["userContent"]})
+            messages.append({"role":"assistant", "content":message["assistantContent"]})
+        messages.append({"role":"user", "content":newUserContent})
+        return messages
+
--- test/test_openai_wraper.py
+++ test/test_openai_wraper.py
@@ -1,8 +1,13 @@
-from src.openai_wraper import ChatInterface, DEFAULT_CAPACITY
+from src.openai_wraper import ChatInterface
+from src.openai_wraper import DEFAULT_CAPACITY, DEFAULT_SYSTEM_PROMPT, DEFAULT_MAX_TOKENS
+from src.openai_wraper import INVALID_CAPACITY_HINT, INVALID_SYSTEM_PROMPT_HINT, INVALID_MAX_TOKENS_HINT, INVALID_USER_CONTENT, INVALID_ASSISTANT_CONTENT
 import pytest
 
-def test_add_message():
-    interface = ChatInterface()
+@pytest.fixture
+def interface():
+    return ChatInterface()
+
+def test_add_message(interface:ChatInterface):
     userContent = "Hello"
     assistantContent = "What\'s up!"
     assert interface.AddMessageToMemory(userContent, assistantContent)
@@ -11,15 +16,23 @@ def test_add_message():
     assert messages[0]["userContent"] == userContent 
     assert messages[0]["assistantContent"] == assistantContent
 
-def test_remove_messages():
-    interface = ChatInterface()
+def test_add_invalid_message(interface:ChatInterface):
+    with pytest.raises(ValueError, match = INVALID_USER_CONTENT):
+        interface.AddMessageToMemory("", "a")
+    messages = interface.GetMessagesFromMemory()
+    assert len(messages) == 0
+    with pytest.raises(ValueError, match = INVALID_ASSISTANT_CONTENT):
+        interface.AddMessageToMemory("a", "")
+    messages = interface.GetMessagesFromMemory()
+    assert len(messages) == 0
+
+def test_remove_messages(interface:ChatInterface):
     assert interface.AddMessageToMemory("Hello", "What\'s up!")
     assert interface.RemoveMessages()
     messages = interface.GetMessagesFromMemory()
     assert len(messages) == 0
 
-def test_add_multiple_message():
-    interface = ChatInterface()
+def test_add_multiple_message(interface:ChatInterface):
     assert interface.SetCapacity(2)
     userContents = ["AB", "CD"]
     assistantContents = ["ab", "cd"]
@@ -31,26 +44,23 @@ def test_add_multiple_message():
         assert messages[index]["userContent"] == userContent 
         assert messages[index]["assistantContent"] == assistantContent
 
-def test_set_memory_capacity():
-    interface = ChatInterface()
+def test_set_memory_capacity(interface:ChatInterface):
     validCapacity = 3
     assert interface.SetCapacity(validCapacity)
     invalidCapacity = -1
-    with pytest.raises(ValueError, match="Invalid Capacity: Capacity must larger than zero!"):
+    with pytest.raises(ValueError, match = INVALID_CAPACITY_HINT):
         interface.SetCapacity(invalidCapacity) 
 
-def test_get_memory_capacity():
-    interface = ChatInterface()
+def test_get_memory_capacity(interface:ChatInterface):
     validCapacity = 3
     invalidCapacity = -1
     assert interface.SetCapacity(validCapacity)
     assert interface.capacity == validCapacity
-    with pytest.raises(ValueError, match="Invalid Capacity: Capacity must larger than zero!"):
+    with pytest.raises(ValueError, match = INVALID_CAPACITY_HINT):
         interface.SetCapacity(invalidCapacity)
     assert interface.capacity == DEFAULT_CAPACITY
     
-def test_add_message_exceed_capacity():
-    interface = ChatInterface()
+def test_add_message_exceed_capacity(interface:ChatInterface):
     assert interface.SetCapacity(1)
     assert interface.AddMessageToMemory("a", "b") 
     assert interface.AddMessageToMemory("c", "d")
@@ -58,4 +68,88 @@ def test_add_message_exceed_capacity():
     assert len(messages) == 1
     assert messages[0]["userContent"] == "c"
     assert messages[0]["assistantContent"] == "d"
-    
+
+def test_set_system_prompt(interface:ChatInterface):
+    assert interface.systemPrompt == DEFAULT_SYSTEM_PROMPT
+    assert interface.SetSystemPrompt("a")
+    assert interface.systemPrompt == "a"
+    assert interface.SetSystemPrompt("b")
+    assert interface.systemPrompt == "b"
+    with pytest.raises(ValueError, match = INVALID_SYSTEM_PROMPT_HINT):
+        interface.SetSystemPrompt("")
+    assert interface.capacity == DEFAULT_CAPACITY
+
+def test_set_max_tokens(interface:ChatInterface):
+    assert interface.maxTokens == DEFAULT_MAX_TOKENS
+    assert interface.SetMaxTokens(1)
+    assert interface.maxTokens == 1
+    assert interface.SetMaxTokens(2048)
+    assert interface.maxTokens == 2048
+    with pytest.raises(ValueError, match = INVALID_MAX_TOKENS_HINT):
+        interface.SetMaxTokens(0)
+    assert interface.capacity == DEFAULT_CAPACITY
+    with pytest.raises(ValueError, match = INVALID_MAX_TOKENS_HINT):
+        interface.SetMaxTokens(2049)
+    assert interface.capacity == DEFAULT_CAPACITY
+
+def test_message_preprocess_systemPrompt(interface:ChatInterface):
+    newUserContent = "hello there"
+    messages = interface.MessagePreprocess(newUserContent)
+    assert len(messages) == 2
+    assert messages[0]["role"] == "system"
+    assert messages[0]["content"] == DEFAULT_SYSTEM_PROMPT
+    assert interface.SetSystemPrompt("test")
+    messages = interface.MessagePreprocess(newUserContent)
+    assert len(messages) == 2
+    assert messages[0]["role"] == "system"
+    assert messages[0]["content"] == "test"
+
+def test_message_preprocess_valid_usercontent(interface:ChatInterface):
+    newUserContent = "hello there"
+    messages = interface.MessagePreprocess(newUserContent)
+    assert len(messages) == 2
+    assert messages[1]["role"] == "user"
+    assert messages[1]["content"] == newUserContent
+    newUserContent = "hello there 2"
+    messages = interface.MessagePreprocess(newUserContent)
+    assert len(messages) == 2
+    assert messages[1]["role"] == "user"
+    assert messages[1]["content"] == newUserContent
+
+def test_message_preprocess_invalid_usercontent(interface:ChatInterface):
+    newUserContent = ""
+    with pytest.raises(ValueError, match = INVALID_USER_CONTENT):
+        messages = interface.MessagePreprocess(newUserContent)
+
+def test_message_preprocess_single_memory(interface:ChatInterface):
+    interface.AddMessageToMemory("a", "b")
+    newUserContent = "hello there"
+    messages = interface.MessagePreprocess(newUserContent)
+    assert len(messages) == 4
+    assert messages[0]["role"] == "system"
+    assert messages[0]["content"] == DEFAULT_SYSTEM_PROMPT
+    assert messages[1]["role"] == "user"
+    assert messages[1]["content"] == "a"
+    assert messages[2]["role"] == "assistant"
+    assert messages[2]["content"] == "b"
+    assert messages[3]["role"] == "user"
+    assert messages[3]["content"] == newUserContent
+
+def test_message_preprocess_multiple_memory(interface:ChatInterface):
+    capacity = 10
+    interface.SetCapacity(capacity)
+    memory = [(str(i), str(i*2)) for i in range(capacity)]
+    for userContent, assistantContent in memory:
+        interface.AddMessageToMemory(userContent, assistantContent)
+    newUserContent = "hello there"
+    messages = interface.MessagePreprocess(newUserContent)
+    assert len(messages) == 2 + capacity * 2
+    for index, (userContent, assistantContent) in enumerate(memory):
+        assert messages[1 + (index * 2)]["role"] == "user"
+        assert messages[1 + (index * 2)]["content"] == userContent
+        assert messages[1 + (index * 2) + 1]["role"] == "assistant"
+        assert messages[1 + (index * 2) + 1]["content"] == assistantContent
+
+# test actual openai api
+
+# the other use unittest Mock openai api
